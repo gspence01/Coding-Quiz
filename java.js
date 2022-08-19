d0889830860f1c192e03d15adc09b0821786efff
Gabrielle <gabrielle01.spence@gmail.com>
2022-08-19 15:30:14 -0700

    Added questions

---
+++ java.js
@@ -0,0 +1,97 @@
+//object for the questions to live in
+let questions = [
+    //Question 1
+    {
+        question: "You might know water bears as what scientific name?",
+        answers: [
+            {option: "1. Tardigrades", value:true},
+            {option: "2. Microfauna", value:false},
+            {option: "3. Rotifers", value:false},
+            {option: "4. Limnognathia", value:false}
+        ]
+    },
+    //Question 2
+    {
+        question: "Why were these little critters named water bears?",
+        answers: [
+            {option: "1. They are brown and fluffy", value:false},
+            {option: "2. Their hibernation cycle resembles that of a bear's", value:false},
+            {option: "3. The way they walk resembles a bear's gait", value:true},
+            {option: "4. Water bears are in the same family as actual bears", value:false}
+        ]
+    },
+    //Question 3
+    {
+        question: "Tardigrades are among the ______ animals.",
+        answers: [
+            {option: "1. Largest", value:false},
+            {option: "2. Meanest", value:false},
+            {option: "3. Hardiest", value:true},
+            {option: "4. Laziest", value:false}
+        ]
+    },
+    //Question 4
+    {
+        question: "Are female tardigrades more common than male tardigrades?",
+        answers: [
+            {option: "1. No, there are more male than female tardigrades", value:false},
+            {option: "2. Yes, there are more female than male tardigrades", value:true},
+            {option: "3. No, there is a 50/50 ratio of male to female tardigrades", value:false},
+            {option: "4. No, tardigrades do not have sexes", value:false}
+        ]
+    },
+    //Question 5
+    {
+        question: "Tardigrades can endure what extremes?",
+        answers: [
+            {option: "1. Tempuratures as low as -200C and as high as 151C", value:false},
+            {option: "2.Boiling alcohol", value:false},
+            {option: "3. Extreme dehydration", value:false},
+            {option: "4. The vacuum of space", value:false},
+            {option: "5. Levels of X-ray radiation 1000x the lethal human dose", value:false},
+            {option: "5. All the answers are correct", value:true}
+        ]
+    },
+    //Question 6
+    {
+        question: "True or false: Tardigrades are true extremophiles",
+        answers: [
+            {option: "1. True", value:false},
+            {option: "2. False", value:true}
+        ]
+    },
+    //Question 7
+    {
+        question: "True or False: Scientists have successfully froze a tardigrade to within a fraction of a degree Kelvin.",
+        answers: [
+            {option: "1. True", value:true},
+            {option: "2. False", value:false}
+        ]
+    },
+    //Question 8
+    {
+        question: "True or False: Tardigrades can suspend their metabolism.",
+        answers: [
+            {option: "1. True", value:true},
+            {option: "2. False", value:false}
+        ]
+    },
+    //Question 9
+    {
+        question: "True or False: Tardigrades don't have eyes",
+        answers: [
+            {option: "1. True", value:true},
+            {option: "2. False", value:false}
+        ]
+    },
+    //Question 10
+    {
+        question: "True or False: Tardigrades can't change their shapes.",
+        answers: [
+            {option: "1. True", value:false},
+            {option: "2. False", value:true}
+        ]
+    }
+
+]
+
